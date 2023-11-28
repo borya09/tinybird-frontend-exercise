34abded77a7eefc51b18f8e18ce4f06450bbf8dc
borja <bandres@plainconcepts.com>
2023-11-28 23:40:00 +0100

    Change logo

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,12 +1,12 @@
 import cn from "classnames";
-import logo from "/logo.svg";
+import logo from "/logo.png";
 import styles from "./Header.module.css";
 
 export default function Header() {
   return (
     <>
-      <a href="/">
-        <img src={logo} alt="tinybird logo" />
+      <a href="/" className={styles.logo}>
+        <img src={logo} className={styles.logo} alt="company logo" />
       </a>
       <i className={cn(styles.avatar, "fa-regular fa-circle-user")}></i>
     </>
